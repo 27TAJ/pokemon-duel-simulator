4b73f9525888e7f3cea0449386204a23a6a1f99e
27TAJ <tommyajones2004@icloud.com>
2024-07-04 05:38:25 -0400

    added possible move getter

--- poke_class.py
+++ poke_class.py
@@ -26,17 +26,32 @@ def generate_team():
 
 class Pokemon:
     def __init__(self, poke_data): # add data as we need it
-        self.id = int(poke_data['id'])
+        self.id = poke_data['id']
         self.name = poke_data['name']
         self.types = poke_data['types']
+        self.moves = []
 
     def __str__(self):
         return f"{self.name}\n (ID: {self.id})" # format info later
     
     #add getters
+    def get_possible_moves(self):
+        url = f"https://pokeapi.co/api/v2/pokemon/{self.id}/"
+
+        try:  
+            response = requests.get(url)
+            response.raise_for_status()
+            poke_data = response.json()
+            possible_moves = poke_data.get('moves', [])
+            move_names = [move['move']['name'] for move in possible_moves]
+            self.moves = random.sample(move_names, min(4, len(move_names)))  #adds 4 or fewer moves to pokemon
+            print(self.moves)
+        except requests.exceptions.RequestException as e:
+            print(f"Error getting possible moves: {e}")
 
     #add functions
 
 my_team = generate_team()
 for p in my_team:
     print(p)
+    p.get_possible_moves()
