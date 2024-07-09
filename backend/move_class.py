d418c72892790277f6717e1742248d969550d656
27TAJ <tommyajones2004@icloud.com>
2024-07-09 19:46:10 -0400

    save before django

--- backend/move_class.py
+++ backend/move_class.py
@@ -10,9 +10,10 @@ class Move:
         self.accuracy = move_data['accuracy']
         self.pp = move_data['pp']
         self.power = move_data['power']
-        self.damage_class = move_data['damage_class']['name']
+        self.damage_class = move_data['damage_class']['name'] # "status", "physical", "or special"
+        
         #stat_change
         #effect_chance
-
+        
     def __str__(self):
        return f"{self.name}"
